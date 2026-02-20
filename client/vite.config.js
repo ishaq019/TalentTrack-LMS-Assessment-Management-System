7b4c21e8fb15b1ffb7f4569d298dd11b4459274c
SYED ISHAQ <229y1a05h4@ksrmce.ac.in>
2026-02-20 17:19:12 +0530

    Update base path for Vite configuration

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,7 +5,7 @@ import path from "path";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/",
+  base: "/TalentTrack-LMS-Assessment-Management-System",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src")
